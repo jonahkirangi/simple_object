711d76963800d692cff7319b183e2a47cea4838c
Jonah Kirangi <jonahkirangi@gmail.com>
2014-02-14 16:19:29 -0800

    Update test file

--- test/card_spec.js
+++ test/card_spec.js
@@ -1,23 +1,24 @@
 var expect = require('chai').expect,
-Card = require('../lib/card').Card;
+  Card = require('../lib/card').Card;
 
 describe('when I create a card with a particular suit and rank', function() {
-
   var myCard;
 
   beforeEach(function() {
     myCard = new Card('heart', 8);
   });
 
-  it('card has that suit and rank', function() {
+  describe('constructor', function() {
+    it('card should exist', function() {
+      expect(myCard).to.be.ok;
+    });
 
-    expect(myCard).to.be.ok;
+    it('card should have particular suit', function() {
+      expect(myCard.getSuit()).to.equal('heart');
+    });
 
-    expect(myCard.getSuit()).to.equal('heart');
-
-    expect(myCard.getRank()).to.equal(8);
-
-
+    it('card should have particular rank', function() {
+      expect(myCard.getRank()).to.equal(8);
+    });
   });
-
 });
