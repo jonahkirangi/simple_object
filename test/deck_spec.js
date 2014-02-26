f7d1ab7ca910e52844454f87be385705cd845589
Jonah Kirangi <jonahkirangi@gmail.com>
2014-02-26 11:48:09 -0800

    Add deck.js and deck_spec.js

---
+++ test/deck_spec.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect,
+  Card = require('../lib/card').Card;
+  Deck = require('../lib/deck').Deck;
+
+describe('when a deck of cards is created', function() {
+  var myDeck;
+
+  beforeEach(function() {
+    myDeck = new Deck(Card);
+  });
+
+  describe('constructor', function() {
+    it('deck should exist', function() {
+      expect(myDeck).to.be.an('object');
+    });
+
+    it('deck should have cards', function() {
+      expect(myDeck).to.have.property('cards');
+    });
+
+    it('deck should store 52 cards', function() {
+      expect(myDeck).to.have.length.equal(52);
+    });
+  });
+});
